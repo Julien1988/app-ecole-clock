0764b22240ae6a4f2395767b9481a3ee437810f3
Julien Broens <julienbroens@gmail.com>
2024-10-16 20:21:20 +0200

    corrections divers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ export default function Home() {
     const [generatedTime, setGeneratedTime] = useState(0);
     const [userInput, setUserInput] = useState(""); // Stocke l'heure entrée par l'utilisateur
     const [isCorrect, setIsCorrect] = useState(false); // Stocke si l'heure entrée par l'utilisateur est correcte
+    const [isAM, setIsAM] = useState(false); // Stocke si l'heure générée est AM ou PM
 
 
     // Générer l'heure aléatoire une seule fois lors du montage du composant
@@ -17,12 +18,15 @@ export default function Home() {
     }, []);
 
     const creatRandomHour = () => {
-
+        setIsAM(false);
         const randomHour = Math.floor(Math.random() * 24); // Génère un nombre aléatoire entre 0 et 23
         const randomMinute = Math.random() < 0.5 ? 0 : 30; // Fixe les minutes à 00 ou 30 aléatoirement
         const fakeDate = new Date(`December 17, 1995 ${randomHour}:${randomMinute}:00`);
         // @ts-ignore
         console.log('fakeDate', fakeDate);
+        if (randomHour < 12) {
+            setIsAM(true);
+        }
         // @ts-ignore
         setGeneratedTime(fakeDate);
 
@@ -51,38 +55,41 @@ export default function Home() {
 
     }
 
+    // @ts-ignore
     return (
         <div
-            className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-            <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+            className="">
+            <main className="flex flex-col min-h-dvh gap-8 items-center justify-center  bg-black">
 
-                <div className={'flex flex-col gap-8'}>
+                <div className={'flex flex-col gap-8 justify-center items-center'}>
 
                     <div>
                         <AnalogTime type={"display"}
                                     value={generatedTime}
-                                    baseColor={"#60a5fa"}
-                                    hourHandColor={"#2563eb"}
-                                    minuteHandColor={"#2563eb"}
+                                    baseColor={"rgb(219 39 119"}
+                                    hourHandColor={"rgb(219 39 119"}
+                                    minuteHandColor={"rgb(219 39 119"}
                                     secondHandColor={"black"}
                                     size={3}
                                     liveUpdater={false}
                         />
                     </div>
                     <div className={"flex flex-col"}>
-                        <label className={'flex gap-2 text-xl'}>
-                            <p>écrit l'heure : </p>
-                            <input placeholder={"12:00"} className={'text-blue-300'} value={userInput}
+                        <label className={'flex gap-2 text-xl flex-col'}>
+                            <p className={"text-center text-2xl first-letter:uppercase"}>écrit l'heure ici :</p>
+                            {isAM && <p className={ 'text-center text-green-500'}>le matin</p>}
+                            {!isAM && <p className={' text-center text-green-500'}>le soir</p>}
+                            <input placeholder={"12:00"} className={'text-blue-300 font-bold h-12 rounded text-center '} value={userInput}
                                    onChange={e => setUserInput(e.target.value)}/>
                         </label>
                         {isCorrect && <p className={'text-green-500'}>Bravo, c'est la bonne heure !</p> &&
                             <a className={" rounded bg-green-400 p-3 my-6 mx-6"} onClick={reset}> essaye encore
                             </a>
                         }
-
-                            <a className={" rounded bg-amber-600 p-3 my-6 mx-6"} onClick={handleClick}> Essaye
+                        {!isCorrect &&
+                            <a className={" rounded bg-rose-900\t p-3 my-6 mx-6 text-center text-2xl hover:bg-pink-600 "} onClick={handleClick}> Essaye
                                 ici</a>
-
+                        }
 
                     </div>
 
